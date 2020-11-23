4324bc03bbed3e5a7de0f27bb1d217bbb1b4e845
Julia Aasmaa <my_e_10@hotmail.com>
2020-11-23 13:32:26 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+const button = document.querySelector('#btn');
+const image = document.querySelector('#img');
+
+button.addEventListener('click', (event) => {
+    console.log('Hello from button click!');
+    console.log(image);
+    image.src = 'img/pic2.jpg';
+    event.preventDefault();
+});
+
 console.log('Hello, World!');
 
 //template literals
@@ -27,4 +37,42 @@ let html = `
 `;
 
 
-document.body.innerHTML = html;
+//document.body.innerHTML = html;
+
+//arrays and arrays methods
+let fruit = ['apples', 'orange', 'bananas'];
+console.log(fruit);
+
+
+
+//add an element to the array
+fruit.push(true);
+
+fruit.unshift('tomato');
+
+
+let orangeIndex = fruit.indexOf('orange');
+console.log('index of orange', orangeIndex);
+
+fruit.splice(orangeIndex, 1);
+
+fruit.forEach(element => {
+    console.log('Hello from fruit', element, typeof(element));
+});
+
+//Date and Time
+
+const today = new Date();
+console.log(today);
+const day = today.getDate();
+const month = today.getMonth();
+const year = today.getFullYear();
+console.log(day, month, year);
+
+let options = {weekday: 'long', month: 'long', year: 'numeric', day: 'numeric'};
+let fullDate = today.toLocaleDateString("en-US", options);
+console.log(fullDate);
+
+let birthday = new Date('December 18 1985');
+let fullBirthday = birthday.toLocaleDateString("en-US", options);
+console.log(fullBirthday);
